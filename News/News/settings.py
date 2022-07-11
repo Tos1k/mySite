8db24ec8d66bec6a137bf57e09266169497e3fd0
Tos1k <2001ghghgh@gmail.com>
2022-07-11 20:07:58 +0400

    db Commit

--- News/News/settings.py
+++ News/News/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*+h-t+#g4vas-e7#i^$8gxpr&-=i5-^chx_$ly4qx+9&)fieo7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- News/blog/views.py
+++ News/blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseNotFound, Http404
+from django.shortcuts import redirect
 
-# Create your views here.
+
+def index(request): #HttpRequest
+    if (request.GET):
+        print(request.GET)
+    return HttpResponse("Домашняя страница")
+
+def categories (request, cat):
+    print (request.GET)
+    return HttpResponse (f"<h1>Статьи по категориям<h1><p>{cat}</p>")
+
+def archive(request, year):
+    if int(year)>2022:
+        raise redirect('home',permanent=True)
+    return HttpResponse(f'<h1> Архив по годам </h1><p>{year}</p>')
+
+def page_not_found (request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
