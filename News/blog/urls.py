dd59faf6ea0c60d9e1c617cc90845fe75067ad1a
Tos1k <2001ghghgh@gmail.com>
2022-07-12 17:54:19 +0400

    bool Commit

--- News/blog/urls.py
+++ News/blog/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path,re_path
+from django.urls import path, re_path
 
 from blog.views import *
 
 urlpatterns = [
-    path('home/', index, name = 'home'),
+    path('home/', index, name='home'),
     path('cats/<slug:cat>', categories),
     re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
 ]
--- News/blog/views.py
+++ News/blog/views.py
@@ -3,18 +3,21 @@ from django.shortcuts import redirect
 
 
 def index(request): #HttpRequest
-    if (request.GET):
+    if request.GET:
         print(request.GET)
     return HttpResponse("Домашняя страница")
 
-def categories (request, cat):
-    print (request.GET)
-    return HttpResponse (f"<h1>Статьи по категориям<h1><p>{cat}</p>")
+
+def categories(request, cat):
+    print(request.GET)
+    return HttpResponse (f"<h1>Статьи по категориям</h1><p>{cat}</p>")
+
 
 def archive(request, year):
-    if int(year)>2022:
-        raise redirect('home',permanent=True)
+    if int(year) > 2022:
+        raise redirect('/home',permanent=True)
     return HttpResponse(f'<h1> Архив по годам </h1><p>{year}</p>')
 
-def page_not_found (request, exception):
+
+def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
